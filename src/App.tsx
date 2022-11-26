9e31c2fafa010075cdcb16ee30fff3350c00b24f
Kei Nakayama <kei.of.nakayama@gmail.com>
2022-11-26 22:12:26 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,15 @@ import {
 import './App.css';
 
 function App() {
-    const [showCreateModal, setShowCreateModal] = useState(false)
-    const [showUpdateModal, setShowUpdateModal] = useState(false)
-    const [updateNote, setUpdateNote] = useState()
+    const [showCreateModal, setShowCreateModal] = useState(false);
+    const [showUpdateModal, setShowUpdateModal] = useState(false);
+    const [updateNote, setUpdateNote] = useState();
     return (
         <>
-            <NavBar width="100%"/>
+            <NavBar
+                width="100%" marginBottom="20px"
+                overrides={{Button31632483: {onClick: () => setShowCreateModal(true)}}}
+            />
             <div className="container">
                 <NoteUICollection/>
             </div>
